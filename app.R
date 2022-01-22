ec4b5c44d80f12817811e16c7fa950bef6994b2e
Hasan Yahya <hasanyah@gmail.com>
2022-01-22 15:32:24 +0100

    Added main part of plot 2, added tabbed view, minor improvements

--- app.R
+++ app.R
@@ -5,7 +5,9 @@ library(shinyalert)
 # Read data
 data = read.csv("data/clean_games.csv")
 data['popularity'] = data['owners']*data['price']
-data['year'] <-  format(as.Date(data$release_date,'%Y-%m-%d'),"%Y")
+data['year'] <- as.integer(format(as.Date(data$release_date,'%Y-%m-%d'),"%Y"))
+data['active_years'] <- 2022 - data['year']
+data['avg_annual_profit'] <- data['popularity']/data['active_years']
 data$release_date <- NULL
 library(maps)
 library(mapproj)
@@ -30,14 +32,14 @@ ui <- fluidPage(
       
       # Input: Slider for the number of bins ----
       sliderInput("year",
-                  label = "Number of bins:",
+                  label = "Games published between:",
                   min = 1997,
                   max = 2019,
                   value = c(1997,2019)),
-      selectInput("developer",
+      selectizeInput("developer",
                   label = "Developer:",
-                  choices = unique(df_data$developer),
-                  selected = 'Valve'),
+                  choices = NULL,
+                  options = list(maxOptions = 30000)),
       radioButtons(inputId="age", label="Age >=", 
                    choices=c(0,16,18), selected=0)
       
@@ -47,18 +49,22 @@ ui <- fluidPage(
     # Main panel for displaying outputs ----
     mainPanel(
       
-      # Output: Histogram ----
-      #plotOutput(outputId = "plot1")
-      plotOutput(outputId = "plot3")
-      
+      tabsetPanel(type = "tabs",
+                  tabPanel("Popularity", plotOutput(outputId = "plot1")),
+                  tabPanel("Revenue", plotOutput(outputId = "plot2")),
+                  tabPanel("Developer Performance", plotOutput(outputId = "plot3")),
+                  tabPanel("Debug Output", verbatimTextOutput("summary"))
+      )
     )
   )
 )
 # Define server logic required to draw a histogram ----
-server <- function(input, output) {
+server <- function(input, output, session) {
+  updateSelectizeInput(session, 'developer', choices = sort(unique(df_data$developer)), selected="Valve", server = TRUE)
+  
 #####PLOT 1###########
   response1 <- reactive({filter(df_data, between(year, input$year[1], input$year[2]))})
-  
+
   output$plot1 <- renderPlot({
     #data['year'] <-  format(as.Date(data$release_date,'%Y-%m-%d'),"%Y")
     #data$release_date <- NULL
@@ -66,17 +72,44 @@ server <- function(input, output) {
 
     separated=separate_rows(data1, genres, sep=";")
     final1 = separated %>%  group_by(year,genres) %>% summarise(owners=sum(owners))
-    
+
     ggplot(final1, aes(x = year, y = owners, fill = genres)) +
       geom_stream()
   })
+  ########PLOT 2#############
+  ######## main #############
+  output$plot2 <- renderPlot({
+    
+    plot2data = data.frame(data[, c('categories', 'genres', 'avg_annual_profit')])
+    plot2data = separate_rows(plot2data, genres, sep=";")
+    plot2data = separate_rows(plot2data, categories, sep=";")
+    
+    usable_data = plot2data %>%  
+      group_by(genres, categories) %>% 
+      summarise(profit=mean(avg_annual_profit)) %>% 
+      spread(genres, profit) %>% 
+      ungroup()
+    usable_data[is.na(usable_data)] <- 0
+    viewable_data <- usable_data %>%
+      gather(colname, value, -categories)
+    ggplot(viewable_data, aes(x = colname, y = categories, fill = value)) +
+      geom_tile() + 
+      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+      labs(x = "Genres", y="Categories", value="Mean Annual Revenue")
+  })
+  
+  
+  
+  ########linked#############
+  
+  
   
   ########PLOT 3#############
 
   response3 <- reactive({df_data %>% filter(developer == input$developer) %>%
       filter(required_age >= input$age)
       })
-  
+
   output$plot3 <- renderPlot({
     data3=response3()
     if(nrow(data3) == 0){
@@ -85,15 +118,21 @@ server <- function(input, output) {
     else{
       data3$price[data3$price == 0.00] <- 1.00
       data3['revenue'] = data3$price * data3$owners
-      
+
       separated=separate_rows(data3, genres, sep=";")
       dev_genre = separated %>%  group_by(genres) %>% summarise(revenue=sum(revenue))
-      ggplot(dev_genre, aes(x=genres, y=revenue )) + 
+      ggplot(dev_genre, aes(x=genres, y=revenue )) +
         geom_bar(stat='identity', width=0.5 ) +
         # geom_text(aes(label=revenue), vjust=1.6,color="white", size=3.5)+
-        scale_color_brewer(palette = "Dark2") 
+        scale_color_brewer(palette = "Dark2")
     }
-    
+  })
+  ##DEBUG OUTPUT - REMOVE WHEN DONE
+  output$summary <- renderText({
+    paste("Current Selections:\n", 
+          "Years: ", input$year[1], "and", input$year[2], "\n",
+          "Developer: ", input$developer, "\n",
+          "Age: ", input$age)
   })
 }
 # Create Shiny app ----
